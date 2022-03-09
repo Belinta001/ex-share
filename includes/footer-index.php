b88c55ec374647ed736b374ff58ee0ec87313e7f
Belinta001 <70023803+Belinta001@users.noreply.github.com>
2022-03-09 11:42:35 -0500

    Add files via upload

---
+++ includes/footer-index.php
@@ -0,0 +1,104 @@
+    <!--footer-area start-->
+    <footer class="footer-area black-bg pos-rel pt-80 pb-50" style="background-image:url(assets/img/bg/02.html)">
+        <div class="container">
+            <div class="row mb-20">
+                <div class="col-xl-3 col-lg-6 col-md-6  wow fadeInUp2 animated" data-wow-delay='.1s'>
+                    <div class="footer__widget mb-30">
+                        <h5 class="semi-02-title mb-25">Quick Links</h5>
+                       <p>But I must explain to you how all this mistaken idea of denouncing pleaure and praising pain was born and will give you a complete account of</p>
+
+                            <div class="footer-log mt-25">
+                            <a href="index.php" class="footer-logo mb-30"><img src="log.png"
+                                    alt="EX-SHARE"></a>
+                        </div>
+
+                 
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-6 col-md-6 wow fadeInUp2 animated" data-wow-delay='.3s'>
+                    <div class="footer__widget fot_abot mb-30 pl-40">
+                        <h5 class="semi-02-title mb-25">Quick Link</h5>
+                        <ul class="fot-list">
+                                            <li><a href="index.php">Home</a></li>
+                         
+                            <li><a href="contact.php">Contact Us</a></li>
+                        
+                            <li><a href="faq.php">FAQs</a></li>
+                                <li><a href="about.php">About Us</a></li>
+
+                      
+                        </ul>
+                    </div>
+                </div>
+              
+                
+                
+                
+                <div class="col-xl-3 col-lg-6 col-md-6  wow fadeInUp2 animated" data-wow-delay='.7s'>
+                    <div class="footer__widget fot_abot mb-30 pl-60">
+                        <h5 class="semi-02-title mb-25">Get In Touch</h5>
+                        <ul class="widget_address_list">
+                            <li>
+                                <div class="widget_address d-flex mb-20">
+                                    <div class="widget_address__icon mr-20">
+                                        <i class="flaticon-location"></i>
+                                    </div>
+                                    <div class="widget_address__content">
+                                        <h4 class="semi-02-title"><a href="contact.php">Location</a></h4>
+                                        <i class="far fa-map-marker-alt"></i> <span>Nairobi - KENYA</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="widget_events d-flex mb-20">
+                                    <div class="widget_address__icon mr-20">
+                                        <i class="flaticon-chat"></i>
+                                    </div>
+                                    <div class="widget_address__content">
+                                        <h4 class="semi-02-title"><a href="contact.php">Email Us</a></h4>
+                                        <span> <a href="mailto:info@sirkellyfoundation.org"> info@ex-share.com</a> </span>
+                                    </div>
+                                </div>
+                            </li>
+                          <!--              <li>
+                                <div class="widget_events d-flex mb-20">
+                                    <div class="widget_address__icon mr-20">
+                                        <i class="flaticon-chat"></i>
+                                    </div>
+                                    <div class="widget_address__content">
+                                        <h4 class="semi-02-title"><a href="contact.php">Postal Address</a></h4>
+                                        <span>  P.O. Box 1373-20200 Kericho </span>
+                                    </div>
+                                </div>
+                            </li> -->
+
+                            <li>
+                                <div class="widget_events d-flex mb-20">
+                                    <div class="widget_address__icon mr-20">
+                                        <i class="flaticon-phone-call"></i>
+                                    </div>
+                                    <div class="widget_address__content">
+                                        <h4 class="semi-02-title"><a href="contact.php">Telephone</a></h4>
+                                        <span>+254 - 740 - 705 - 360</span>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul> 
+                    </div>
+                </div>
+            </div>
+            <!--scroll-target-btn-->
+            <!-- <a href="#top-menu" class="scroll-target"><i class="far fa-arrow-up"></i></a> -->
+            <!--scroll-target-btn-->
+            <div class="copy-right-area pt-30">
+                <div class="row align-items-center">
+                    <div class="col-xl-12">
+                        <div class="copyright text-center">
+                            <p>Copyright © 2022 | <span> EX-SHARE </span>| All rights reserved.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--footer-area end-->
---
+++ includes/footer-rest.php
@@ -0,0 +1,73 @@
+    <!--footer-area start-->
+    <footer class="footer-area black-bg pos-rel pt-80 pb-50" style="background-image:url(assets/img/bg/02.html)">
+        <div class="container">
+            <div class="row mb-20">
+                <div class="col-xl-3 col-lg-3 col-md-6  wow fadeInUp2 animated" data-wow-delay='.1s'>
+                    <div class="footer__widget mb-30">
+                        <h5 class="semi-02-title mb-25">Our Mission</h5>
+                                                            <div class="inner-content__text">
+                                            <p>
+To consistently add value to society by meticulously crafting strategies for their sustainable growth, supporting the continuous development of our human talent, and strengthening and expanding access to education, career and business opportunities.
+</p>
+                                        </div>
+
+                  <!--       <div class="footer-log mt-25">
+                            <a href="index.php" class="footer-logo mb-30"><img src=""
+                                    alt="EX-SHARE"></a>
+                        </div> -->
+                       
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-6 wow fadeInUp2 animated" data-wow-delay='.3s'>
+                    <div class="footer__widget fot_abot mb-30 pl-40">
+                        <h5 class="semi-02-title mb-25">Quick Link</h5>
+                        <ul class="fot-list">
+                            <li><a href="index.php">Home</a></li>
+                         
+                            <li><a href="contact.php">Contact Us</a></li>
+                        
+                            <li><a href="faq.php">FAQs</a></li>
+                                <li><a href="about.php">About Us</a></li>
+
+                    
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-6  wow fadeInUp2 animated" data-wow-delay='.5s'>
+                    <div class="footer__widget fot_abot mb-25">
+              
+                <div class="footer-log mt-25">
+                            <a href="index.php" class="footer-logo mb-30"><img src="log.png"
+                                    alt="EX-SHARE"></a>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-6  wow fadeInUp2 animated" data-wow-delay='.7s'>
+                    <div class="footer__widget fot__subscribe mb-30">
+                        <h5 class="semi-02-title mb-25">Our Newsletters</h5>
+                        <p class="mb-25">To receive our monthly newspapers</p>
+                        <div class="form-area mb-30">
+                            <form action="#" method="post">
+                                <input type="text" class="form-control" placeholder="Enter Your Email">
+                                <button class="theme_btn theme_btn_bg mt-10">subscribe now <span><i class="fas fa-envelope"></i></span></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--scroll-target-btn-->
+            <!-- <a href="#top-menu" class="scroll-target"><i class="far fa-arrow-up"></i></a> -->
+            <!--scroll-target-btn-->
+            <div class="copy-right-area copy-area-02 pt-30">
+                <div class="row align-items-center">
+                    <div class="col-xl-12">
+                        <div class="copyright text-center">
+                            <p>Copyright © 2022 | <span> EX-SHARE </span> | All rights reserved.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--footer-area end-->
